d0198cc6e7af10a7d6fbe78a511593ecb7eac1a7
Tim <tim@Macbook-Pro>
2017-08-11 16:35:48 +0200

    cleaned up package

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,25 @@
-import {NgModule} from "@angular/core";
-import {CommonModule} from "@angular/common";
-import {Tooltip} from "./Tooltip";
-import {TooltipContent} from "./TooltipContent";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { Modal, ModalContent, ModalFooter, ModalHeader } from './modal/modal.component'
+import {
+	Modal,
+	ModalContent,
+	ModalFooter,
+	ModalHeader
+} from './modal/modal.component'
 
-export * from "./Tooltip";
-export * from "./TooltipContent";
 export * from "./modal/modal.component";
 
 @NgModule({
-    imports: [
-        CommonModule
-    ],
-    declarations: [
-        Tooltip,
-		TooltipContent,
+	imports: [
+		CommonModule
+	],
+	declarations: [
 		Modal, ModalContent, ModalFooter, ModalHeader,
-    ],
-    exports: [
-        Tooltip,
-		TooltipContent,
+	],
+	exports: [
 		Modal, ModalContent, ModalFooter, ModalHeader,
-    ],
-    entryComponents: [
-        TooltipContent
-    ]
+	]
 })
 export class TooltipModule {
 
